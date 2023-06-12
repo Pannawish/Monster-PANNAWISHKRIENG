352b0edc012652eb6ad9a4c7b60d7267d7bb1829
Pannawish <132990555+Pannawish@users.noreply.github.com>
2023-06-12 17:03:43 +0000

    submit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,112 @@
 #include <iostream>
 using namespace std;
-#include"monster.h"
+#include "Thanos.h"
 
 int main() {
-  monster m("Strange",10,20);
-  //monster x[10]={{"JJ",20,20},{"Burin"}};
-  monster a("JJ",20,20),b("Sittipoj");
+
+ /* monster x("Apple", 10, 20), y("Boba",5,5);// call constructors
+
+  x.print();
+  y.print();
+
+  int i;
+  x.Attack(y);
+  x.print();
+  y.print();
+ 
+  monster *p=&x;
+   while(p->not_dead())
+      p->Attack(y);//x.Attack(y);
   
-    monster *p;
-  p=new monster[3];
-  // how to create a monster object for p
-  // how to delete 
-  delete []p;
+  x.print();
+  y.print();
+*/
+ /* monster *p,x("Big",10,10); // constructor 1(big)
+  p=&x;
+  
+  x+=30; //x.operator+=(30);
+  --x;
+  
+  p=new monster("Boat",5,20);// constructor 2(boat)
+  while(p->not_dead() && x.not_dead()){
+    p->Attack(x);
+    x.Attack(*p);
+  }
+  p->print();
+  x.print();
+ delete p;   //destructor 1(boat)
+  */
+  
+/*
+ p=new monster[3];//constructor 3, 4,5(not, bond, top)
+ delete []p; //destructor (top, bond,not)
+*/
+  //destructor big
+  
+  
+  // x.hp decrease by 1 y.hp decrese by half
+ 
+  /* monster xx[5] = {{"Bright", 30}, {"Big"}, {"Boat"}};
+  monster *p = new monster("Mike");
+  delete p;*/
+
+
+
+  /* Assignment*/
+
+  Thanos T;
+  int n;
+  cout<<"How many monster?";
+  cin>>n;
+
+  monster *m=new monster[n]; //2. make sure they have hp
+ 
+  /* Add some story Attack each other or 
+
+  add hp to some of them randomly
+*/
+  int i;
+  for(i=0;i<n;i++){
+    cout<<"Monster #"<<i+1<<endl;
+    (*(m+i)).set_all();
+  }
+  for(i=0;i<n;i++)
+      m[i].print();
+  
+    //saint buff monster's hp randomly for n times
+  for(i=0;i<n;i++){
+    int k=rand()%n;
+    m[k].set_hp((rand()%1000));
+  }
+  for(i=0;i<n;i++)
+      m[i].print();
+  //chaos! monster start attacking each other.
+while(1){
+ int a=rand()%n;
+  int b=rand()%n;
+  if (a!=b&&((*(m+a)).get_hp()!=0)&&((*(m+b)).get_hp()!=0)) 
+    (*(m+a)).Attack(*(m+b));
+
+  if ((rand()%100)<10) break;
+}
+  for(i=0;i<n;i++)
+      m[i].print();
+  
+  ++T;
+  ++T;
+  T.snap_finger(m,n); //finish the snap finger function
+  ++T;
+  ++T;
+  T.snap_finger(m,n);
+  ++T;
+  ++T;
+  T.snap_finger(m,n);
+
+
+  for(i=0;i<n;i++)
+      m[i].print();
+  
+  delete [] m;
+  
+
 }
--- monster.h
+++ monster.h
@@ -1,22 +1,72 @@
+#ifndef monster_h
+#define monster_h
+
 class monster{
 private:
 	string name;
 	int hp,potion;
 public:
-	/*void Attack(moster &);
-          void heal();*/
-	monster(string ="Bob", int =30,int=69420);
-  ~monster();
-};
-monster::~monster(){
-   cout<<"==========="<<endl;
-   cout<<name<<"Destroyed"<<endl;
-}
+int get_hp(){return hp;}
+void operator++();
+void operator--();
+void operator+=(int); // += hp
+void set_hp(int a){hp=a;}
+void Attack(monster &); // A attack B  A.hp--; B.hp/=2 
+//void heal(); // your idea to help revive hp
+void print(); // to print all data
+bool not_dead() { return hp>0;  }
+void set_all();
+string get_name(){return name;}
+monster(string, int=0,int=0);
+monster();
+//Alt+126
+~monster();
 
-monster::monster(string n,int h,int p){
-  name=n;
-  hp=h;
-  potion=p;
-  cout<<name<<" created"<<endl;
+};
+
+
+
+void monster::print(){
+  cout<<"Monster "<<name;
+  cout<<" hp:"<<hp<<" potion:   "<<potion<<endl;
   
 }
+void monster::Attack(monster & b){// x.Attack(y)
+     cout<<"Monster "<<name<<" attack monster "<<b.get_name()<<endl;
+     if(hp>b.get_hp()){
+         // Some action?
+       b.set_hp(0);
+       cout<<"Monster "<<b.get_name()<<" is dead"<<endl;
+     }
+  else cout<<"Monster "<<name<<" is dead"<<endl;
+  hp=0;
+  
+}
+
+monster::~monster(){
+  //indicate which monster is gone
+}
+monster::monster(){
+ /* Ask for input */
+}
+monster::monster(string n ,int p,int h){
+
+  //set var
+  name=n;
+  hp=h ;
+  potion=p;
+  cout<<"monster "<<name <<" is here HAHAHA"<<endl;
+  
+}
+void monster::set_all(){
+  cout<<"Name: ";
+  cin>>name;
+  cout<<"HP: ";
+  cin>>hp;
+  cout<<"Potion: ";
+  cin>>potion;
+  cout<<endl;
+}
+
+
+#endif
